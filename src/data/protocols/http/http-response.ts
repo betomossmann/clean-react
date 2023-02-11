c37505dcbd2e2b1dbc3beba338c100547df07c9f
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-02-11 11:13:19 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -2,9 +2,8 @@ export enum HttpStatusCode {
   ok = 200,
   noContent = 204,
   badRequest = 400,
-  forbidden = 403,
-  notFound = 404,
   unauthorized = 401,
+  notFound = 404,
   serverError = 500
 }
 
