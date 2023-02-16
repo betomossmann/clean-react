80e55280ada17daf432130137ab4425852deccf9
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-02-16 07:49:58 -0300

    refactor: add index files with multiple imports

---
+++ public/js/main.0b756d1db809fdeb2ad7.hot-update.js
@@ -0,0 +1,40 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateclean_react"]("main",{
+
+/***/ "./src/main/index.tsx":
+/*!****************************!*\
+  !*** ./src/main/index.tsx ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"react-dom\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _presentation_pages__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/presentation/pages */ \"./src/presentation/pages/index.ts\");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default().render(react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_presentation_pages__WEBPACK_IMPORTED_MODULE_2__.Login, null), document.getElementById('main'));\n\n\n//# sourceURL=webpack://clean-react/./src/main/index.tsx?");
+
+/***/ }),
+
+/***/ "./src/presentation/pages/index.ts":
+/*!*****************************************!*\
+  !*** ./src/presentation/pages/index.ts ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Login\": () => (/* reexport safe */ _login_login__WEBPACK_IMPORTED_MODULE_0__[\"default\"])\n/* harmony export */ });\n/* harmony import */ var _login_login__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./login/login */ \"./src/presentation/pages/login/login.tsx\");\n\n\n\n//# sourceURL=webpack://clean-react/./src/presentation/pages/index.ts?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("27c13bdb08724f185c0e")
+/******/ })();
+/******/ 
+/******/ }
+);
