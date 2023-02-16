df71d169351fbf763eb81e17c7b363c6f5f79114
Gilberto Mossmann <gilbertomossmann@gmail.com>
2023-02-16 13:55:58 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import React from "react";
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import Login from './login'
+import { Validation } from "@/presentation/protocols/validation";
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
 }
 
-const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
-  return {
-    sut
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate( input: object ): string {
+    this.input = input
+    return this.errorMessage
   }
 }
 
-describe('Login Component', () => {
-  test('Should start with initial state', () => {
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+describe("Login Component", () => {
+  afterEach(cleanup)
+
+  test("Should start with initial state", () => {
     const { sut } = makeSut()
-    const errorWrap = sut.getByTestId('error-wrap')
+    const errorWrap = sut.getByTestId("error-wrap")
     expect(errorWrap.childElementCount).toBe(0)
-    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId("submit") as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
-    const emailStatus = sut.getByTestId('email-status')
-    expect(emailStatus.title).toBe('Campo obrigatório')
-    expect(emailStatus.textContent).toBe('•')
-    const passwordStatus = sut.getByTestId('password-status')
-    expect(passwordStatus.title).toBe('Campo obrigatório')
-    expect(passwordStatus.textContent).toBe('•')
+    const emailStatus = sut.getByTestId("email-status")
+    expect(emailStatus.title).toBe("Campo obrigatório")
+    expect(emailStatus.textContent).toBe("•")
+    const passwordStatus = sut.getByTestId("password-status")
+    expect(passwordStatus.title).toBe("Campo obrigatório")
+    expect(passwordStatus.textContent).toBe("•")
+  })
+
+  test("Should call Validation with correct email", () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
+
+
+
+// const passwordInput = sut.getByTestId('email')
+// fireEvent.input(passwordInput, { target: { value: 'any_password' } })
